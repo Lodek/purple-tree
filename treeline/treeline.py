fce5d39fefe098f11f8b44a6b72bffd4aad54a65
Lodek <23224552+Lodek@users.noreply.github.com>
2018-02-22 21:34:23 -0300

    really fixed recusion problem this time, changed the format of org file and cleaned up treeline.py a little bit

--- treeline/treeline.py
+++ treeline/treeline.py
@@ -7,31 +7,34 @@ it also uses wget to download any favorites that aren't in the .treeline/favorit
 from model import Node, Note
 from config import *
 import os
-marks_p = target_p+'/.treeline/marks'
 
 
-def gen_files(files):
-    root = db_session.query(Node).filter(Node.url=='root').first()
-    recursive(root,1,files)
 
-def recursive(node,level,files):
-    """ some recursive action to generate html and org tree files
-        essentially it calls this function for each child in a node until there is no child """
-    files[1].write(('*'*level+' [[{}][{}]]    '.format(node.url,node.title)))
-    files[1].write('[M]') if node.mark is True else ''
-    files[1].write('[O]') if node.in_session is True else ''
-    files[1].write('\n')
-    for note in node.notes:
-        files[1].write('{}\n'.format(note.body))
-    if node.childs == []:
-        return
-    else:
+def gen_org_tree():
+    org_fp = target_p+'/treeline.org'
+    org_f = open(org_fp, 'w')
+    nodes = db_session.query(Node).order_by(Node.id).all()
+    elisp_link = lambda url,title: '*** [[elisp:(treeline-goto-node "{}" "{}")][{}]]\n'.format(url,title,title)
+    for node in nodes:
+        org_f.write('* [[{}][{}]]    '.format(node.url, node.title))
+        if node.mark is True:
+            org_f.write('[M]')
+        if node.in_session is True:
+            org_f.write('[O]')
+        org_f.write('\n')
+        org_f.write('** notes\n')
+        for note in node.notes:
+            org_f.write(note.body+'\n')
+        org_f.write('** parents\n')
+        for parent in node.parents:
+            org_f.write(elisp_link(parent.url, parent.title))
+        org_f.write('** children\n')
         for child in node.childs:
-            if child not in node.parents:
-                recursive(child, level+1,files)
-
+            org_f.write(elisp_link(child.url, child.title))
+        
 
 def dl_marks():
+    marks_p = target_p+'/.treeline/marks'
     marks = db_session.query(Node).filter(Node.mark==True).all()
     for mark in marks:
         folder=marks_p+'/'+str(mark.id)
@@ -39,33 +42,20 @@ def dl_marks():
             os.makedirs(folder)
             os.system("wget -k -E -p -P {} '{}'".format(folder,mark.url))
 
-def gen_notes(files):
-    notes = db_session.query(Note).all()
-    if notes is None:
-        return
-    for note in notes:
-        files[2].write('* {}\n{}'.format(note.node.url,note.body))
+def gen_notes():
+    notes_fp = target_p+'/treeline-notes.org'
+    with open(notes_fp,'w') as notes_f:
+        notes = db_session.query(Note).all()
+        if notes is None:
+            return
+        for note in notes:
+            notes_f.write('* {}\n{}'.format(note.node.url,note.body))
         
 
-def open_files():
-    html_fp = target_p+'/treeline.html'
-    org_fp = target_p+'/treeline.org'
-    notes_fp = target_p+'/treeline-notes.org'
 
-    html_f = open(html_fp,'w')
-    org_f = open(org_fp,'w')
-    notes_f = open(notes_fp,'w')
-    return (html_f, org_f, notes_f)
-
-
-def close_files(files):
-    for f in files:
-        f.close()
         
 def update():
-    files=open_files()
-    gen_files(files)
-    gen_notes(files)
+    gen_org_tree()
+    gen_notes()
     dl_marks()
-    close_files(files)
     return
